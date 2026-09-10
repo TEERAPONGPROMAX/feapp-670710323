6f21c8d2457b725bff0065f9fe73589b3e6f9184
Teerapong Somboon <SOMBOON_T4@su.ac.th>
2026-09-10 02:42:08 +0000

    component

--- ball-app/src/App.js
+++ ball-app/src/App.js
@@ -1,5 +1,5 @@
-import Navbar from './compenents/Navbarr';
-import Footer from './compenents/Footer';
+import Navbar from './components/Navbarr';
+import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
